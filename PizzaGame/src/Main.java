82139b72727953b9ed3b38fb1db9afac81ad753c
(Arc)Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2026-02-27 17:40:13 +0800

    Noel First Commit

--- PizzaGame/src/Main.java
+++ PizzaGame/src/Main.java
@@ -3,11 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        System.out.println("Noel is online... ");
 
-        System.out.println("Testing");
-        int num = sc.nextInt();
-
-        System.out.println(num);
     }
 }
