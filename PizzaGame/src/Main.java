9b1f56b3301bb16c4167ff2ad9b77f6ed12f9b18
V <akanekei222@gmail.com>
2026-02-27 22:12:35 +0800

    second commit

--- PizzaGame/src/Main.java
+++ PizzaGame/src/Main.java
@@ -1,9 +1,10 @@
-import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
 
         System.out.println("Noel is online... ");
+        System.out.println("Ivy is online...");
 
     }
 }
